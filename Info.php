84cf8e9d7a88e269ea38a1234b4dba3cbb80844e
Irais Gopar <igopar-osorio@csumb.edu>
2018-02-19 06:58:26 +0000

    Finished?

---
+++ Info.php
@@ -0,0 +1,56 @@
+<html>
+<head>
+        <meta charset = "utf-8"/>
+        <title>Irais Gopar: Random website</title>
+        
+        <link href ="style/style.css" rel =" stylesheet" type = "text/css"/>
+    </head>
+    
+    
+    <body>
+        <header>
+            <h1 style="font-size:60px;">Music of the Day Plus a Quote</h1>
+        </header>
+        <nav>
+            <hr width ="50%">
+            <a href ="index.php">Play</a>
+            <a href = "Info.php"><i>Song Information</i></a>
+        </nav>
+        <!--num = rand(1,3);-->
+        <!--num = rand (0,3);-->
+
+       
+    <?php
+    
+    display();
+    
+    function display()
+    {
+        $Ssongs = array("<p>Awaiting her Arrival</br> <i>Alex Barry</i></p>","<p>N°034 </br><i>- Relaxdaily</i></p>", "<p>Moonrise </br><i>-Brian Crain</i></p>","<p>River Flows in you </br> <i> -Yiruma</i></p>","<p>Stay With Me Piano Cover</br><i>-Unknown</i></p>");
+        $rSongs =array("<p>Danza Kuduro </br><i>-Don Omar</i></p>","<p>How Far I'll Go </br><i>-Alessia Cara</i></p>","<p>Dragostea Din Tei</br><i>~O-Zone</i></p>","<p>Feel This Moment </br> - Pitbull ft Christina Aguilera</p>","<p>Let Her Go </br> <i> Passenger</i></p>");
+        echo "<h1 class = rSongs>Song Names and Artist of Random Playlist: </br></br> </h1>";
+        for($i = 0; $i<10;$i++)
+        {
+            echo $rSongs[$i];
+        }
+        echo "<h1 class = sSongs>Song Names and Artist of Study Playlist: </h1>";
+        for($i = 0; $i<10;$i++)
+        {
+            echo $Ssongs[$i];
+        }
+        
+    }
+        
+    ?>
+        </body>
+        <footer id="fter">
+            <hr>
+            CST 336. 2018&copy; Gopar<br />
+            <strong>Disclaimer:</strong> This website is used for academic purposes only.<br />
+            <img src="https://csumb.edu/sites/default/files/styles/profile_large/public/photo_1_6.jpg?itok=N4I4g9Wl">
+        </footer>
+    <!--<button type="Study">Study!</button>-->
+
+
+
+</html>
--- index.php
+++ index.php
@@ -14,6 +14,11 @@
         <header>
             <h1 style="font-size:60px;">Music of the Day Plus a Quote</h1>
         </header>
+        <nav>
+            <hr width ="50%">
+            <a href ="index.php"><i>Play new Song</i></a>
+            <a href = "Info.php">Song Information</a>
+        </nav>
         <!--num = rand(1,3);-->
         <!--num = rand (0,3);-->
 
@@ -24,13 +29,19 @@
         // $quotes = array("Believing in yourself.","Cultivating a success mindset.", "Overcoming procrastination.", "Not making excuses.");
         
         
-        function playStudy($num, $x)
+        function playStudy($num, $x, $y)
         {
             echo "<h1>You are listening to the Study playList!</h1> </br> </br>";
+            $pics = array("bridge.jpg", "buck-full-moon.jpg","cherryBlossem.jpg","korea.jpg","lowTides.jpg","paris2.jpg","rain.png","snow.png","waterfall.gif");
             $study = array('1.mp3','2.mp3','3.mp3','4.mp3','5.mp3');
             $quotes = array("<p>Whatever you are, be a good one.</br><i>-Abraham Lincoln</i></p>","<p>If you dream it, you can do it.</br><i>-Walt Disney</i></p>", "<p>Never, never, never give up.</br><i>-Winston Churchill.</i></p>", "<p>Action is the foundational key to all success.</br><i>-Pablo Picasso</i></p>");
-            echo "<h2>Your random quote is: </h2></br> </br>";
+           
+            
+            echo '<div class = "pics"><img src="pics/'.$pics[$y].'"></div>';
+            
+             echo "<h2>Your random quote is: </h2></br> </br>";
             echo $quotes[$x];
+            
           echo "<audio autoplay loop>
                     <source src= music/study/$study[$num] type=audio/mpeg>
                     Your browser does not support the audio element.
@@ -42,22 +53,20 @@
            // echo $quotes[0];
                     
         }
-        function playRandom($num, $x)
+        function playRandom($num, $x, $y)
         {
             echo "<h1>You are listening to the Random Playlist! Enjoy!</h1> </br> </br>";
-            $pics = array("bridge.jpg", "buck-full-moon.jpg","cherryBloddem.jpg","korea.jpg","lowTides.jpg","paris2.jpg","rain.png","snow.png","waterfall.gif");
+            $pics = array("bridge.jpg", "buck-full-moon.jpg","cherryBlossem.jpg","korea.jpg","lowTides.jpg","paris2.jpg","rain.png","snow.png","waterfall.gif");
             $random = array('music/random/1.mp3','music/random/2.mp3','music/random/NumaNuma.mp3','music/random/HowFarIllGo.mp3','music/random/DanzaKuduro.mp3');
             //$i = rand(0,3);
             $q2 = array("<p>Be yourself, Everyone else is already taken. </br><i> -OSCAR WILDE</i></p>","<p>SOMETIMES YOU WAKE UP. SOMETIMES THE FALL KILLS YOU. AND SOMETIMES, WHEN YOU FALL, YOU FLY. </br> <i> -NEIL GAIMAN, THE SANDMAN</i></p>", "<p>If you set your goals ridiculously high and it is a failure, you will fail above everyone elses success.</br><i> -James Cameron</i></p>","<p>Be nice to people on your way up because you meet them on your way down.</br><i>-Jimmy Durante</i></p>" );
+           echo '<div class = "pics"><img src="pics/'.$pics[$y].'"></div>';
            echo "<h2>Random quote of the day: </h2></br>".$q2[$x];
-           for($i = 0; $i < 9; $i++){
-               if($i == 0)
+           
                 echo "<audio autoplay loop>
                     <source src= $random[$num] type=audio/mpeg>
                     Your browser does not support the audio element.
                     </audio>";
-            echo '<img src="pics/'.$pics[$i].'">';
-           }
         }
         // function picture()
         // {
@@ -69,17 +78,18 @@
             $num = rand(0,4);
             $x = rand(0,3);
             $c = rand(0,1);
+            $y = rand(0,8);
             
             if($c == 0)
-                playStudy($num, $x);
+                playStudy($num, $x, $y);
             else
-                playRandom($num, $x);
+                playRandom($num, $x, $y);
             
         }
     ?>
     
     </body>
-     <footer id="fter">
+        <footer id="fter">
             <hr>
             CST 336. 2018&copy; Gopar<br />
             <strong>Disclaimer:</strong> This website is used for academic purposes only.<br />
